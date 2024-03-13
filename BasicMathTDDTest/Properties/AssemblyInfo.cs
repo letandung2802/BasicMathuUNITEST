d4be73e00bc901ff5d010cc60dc725b656a7b0f6
Le Tan Dung <letandungnq@gmail.com>
2024-03-13 10:56:43 +0700

    ttdtest

---
+++ BasicMathTDDTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicMathTDDTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicMathTDDTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c39b9b8d-6570-4a27-9049-b193fe6b11f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BasicMathTDDTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BasicMath;
+namespace BasicMathTDDTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(20.50, 20.50);
+            Assert.AreEqual(41, res);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(100, res);
+        }
+    }
+}
